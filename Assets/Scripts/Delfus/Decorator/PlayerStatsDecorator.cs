05e9e09a7b12d6f8c92a7d360aa4f807df324f58
felipechurio <felipe.churio@davinci.edu.ar>
2025-12-15 04:12:29 -0300

    InPrigewss

---
+++ Assets/Scripts/Delfus/Decorator/PlayerStatsDecorator.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerStatsDecorator : IPlayerStats
+{
+    protected IPlayerStats _innerStats;
+
+    protected PlayerStatsDecorator(IPlayerStats innerStats)
+    {
+        _innerStats = innerStats;
+    }
+
+    public virtual float GetVelocity() => _innerStats.GetVelocity();
+    public virtual float GetJumpForce() => _innerStats.GetJumpForce();
+}
+
