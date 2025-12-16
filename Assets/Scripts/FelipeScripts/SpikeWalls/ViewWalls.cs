c2ff8dc3f1bf603668e7168815de82f8ffb2cd1c
felipechurio <felipe.churio@davinci.edu.ar>
2025-12-16 04:14:34 -0300

    MiniFIc

--- Assets/Scripts/FelipeScripts/SpikeWalls/ViewWalls.cs
+++ Assets/Scripts/FelipeScripts/SpikeWalls/ViewWalls.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ViewWalls : ISound,IAnimation
+public class ViewWalls : ISound
 {
 
     private AudioSource _audio;
